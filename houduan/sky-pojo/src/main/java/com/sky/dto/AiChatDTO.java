908df4e895dc8bc9496d2a3dc230b37bb6056ad2
lishenghui <1474315986@qq.com>
2026-08-25 17:46:47 +0800

    feat: add AI chat request and response models

---
+++ houduan/sky-pojo/src/main/java/com/sky/dto/AiChatDTO.java
@@ -0,0 +1,17 @@
+package com.sky.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiChatDTO implements Serializable {
+
+    private String message;
+}
---
+++ houduan/sky-pojo/src/main/java/com/sky/vo/AiChatVO.java
@@ -0,0 +1,17 @@
+package com.sky.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiChatVO implements Serializable {
+
+    private String answer;
+}
